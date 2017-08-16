b45c1463679ed31a2f76f8052c43b18faeacd497
andrada0thea <vaduvoiu.teona@gmail.com>
2017-08-16 20:43:30 +0100

    doing tutorial

--- learnyouahaskell.hs
+++ learnyouahaskell.hs
@@ -1,5 +1,7 @@
 -- tutorial functions
 -- http://learnyouahaskell.com
+import qualified Data.Map as Map
+
 removeNonUppercase :: [Char] -> [Char]
 removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
 
@@ -315,3 +317,23 @@ infixr 5 .++
 (.++) :: List a -> List a -> List a
 Empty .++ ys = ys
 (x :-: xs) .++ ys = x :-: (xs .++ ys)
+
+data Tree a = EmptyTree | Node a (Tree a) (Tree a) deriving (Show, Read, Eq)
+
+singleton :: a -> Tree a
+singleton x = Node x EmptyTree EmptyTree
+
+treeInsert :: (Ord a) => a -> Tree a -> Tree a
+treeInsert x EmptyTree = singleton x
+treeInsert x (Node a left right)
+ | x == a = Node x left right
+ | x < a = Node a (treeInsert x left) right
+ | x > a = Node a left (treeInsert x right)
+
+treeElem :: (Ord a) => a -> Tree a -> Bool  
+treeElem x EmptyTree = False  
+treeElem x (Node a left right)  
+    | x == a = True  
+    | x < a  = treeElem x left  
+    | x > a  = treeElem x right 
+
