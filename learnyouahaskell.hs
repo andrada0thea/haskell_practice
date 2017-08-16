e46c1305ca8870df5fb73ce73e4221f10df6ab66
andrada0thea <vaduvoiu.teona@gmail.com>
2017-08-16 20:29:08 +0100

    doing tutorial

--- learnyouahaskell.hs
+++ learnyouahaskell.hs
@@ -306,3 +306,12 @@ inPhoneBook name pnumber pbook = (name, pnumber) `elem` pbook
 
 data Either a b = Left a | Right b deriving (Eq, Ord, Read, Show)  
 
+-- data List a = Empty | Cons a (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 :-:
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
+
+infixr 5 .++
+(.++) :: List a -> List a -> List a
+Empty .++ ys = ys
+(x :-: xs) .++ ys = x :-: (xs .++ ys)
