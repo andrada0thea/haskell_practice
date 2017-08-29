163b27313444ee0374615ffc39d563e49fbb6939
andrada0thea <vaduvoiu.teona@gmail.com>
2017-08-29 15:22:35 +0100

    doing tutorial

--- learnyouahaskell.hs
+++ learnyouahaskell.hs
@@ -356,3 +356,28 @@ instance Show TrafficLight where
   show Yellow = "Yellow light"
   show Green = "Green light"
 
+class YesNo a where
+  yesno :: a -> Bool
+
+instance YesNo Int where
+  yesno 0 = False
+  yesno _ = True
+
+instance YesNo [a] where
+  yesno [] = False
+  yesno _ = True
+
+instance YesNo Bool where
+  yesno = id
+
+--instance YesNo (Maybe a) where
+--  yesno (Just _) = True
+--  yesno Nothing = False
+
+instance YesNo (Tree a) where
+  yesno EmptyTree = False
+  yesno _ = True
+
+instance YesNo TrafficLight where
+  yesno Red = False
+  yesno _ = True
