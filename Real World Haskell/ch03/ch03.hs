a7b4b0efbff7798072077e674ac5d66e5058e974
andrada0thea <vaduvoiu.teona@gmail.com>
2017-09-18 15:49:46 +0100

    reading chapter 3

--- Real World Haskell/ch03/ch03.hs
+++ Real World Haskell/ch03/ch03.hs
@@ -2,3 +2,18 @@
 length' :: Num a => [a1] -> a
 length' [] = 0
 length' (x:xs) = 1 + length' xs
+
+-- exercise 3
+meanListElements :: Foldable t => t Int -> Int
+meanListElements x = sum x `div` (length x)
+
+-- exercise 4
+makePalindrome :: [a] -> [a]
+makePalindrome x = x ++ reverse x
+
+-- exercise 5
+checkPalindrome :: Eq a => [a] -> Bool
+checkPalindrome x
+ | x == reverse x = True
+ | x /= reverse x = False
+
